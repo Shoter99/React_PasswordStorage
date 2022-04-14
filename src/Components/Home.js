00197ef6a565bb0067c570a4458050e40c80ea38
Vivi <shoter998@gmail.com>
2022-04-14 17:30:27 +0200

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
 
   }
   const check_pin = () => {
-    var new_pin = window.prompt('Set new pin: ')
+    var new_pin = window.prompt('Enter pin pin: ')
     if (new_pin == pin) return true
     alert('Wrong pin')
     return false
@@ -123,7 +123,6 @@ const Home = () => {
               </div>
             </form>
           </div>
-          <button onClick={ handle_pin }>Get pin</button>
         </div>
         <div className="grid-wrapper">
           {data.map((item, index) => <GridElement key={index} data={item} deleteItem={deleteItem} check_pin={check_pin} editItem={editItem} decrypt={decrypt}/>)}
